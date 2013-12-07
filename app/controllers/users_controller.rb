a0ed61afdb5c8a3400dee22717f791b5d5cb8c56
Brid Mackey <brid.mackey@gmail.com>
2013-12-07 23:24:03 +0000

    Finishing signin!!! ;)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the NoteIt Microblog!"
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 NoteIt::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root  to: 'static_pages#home'
 
-  match '/home',    to: 'static_pages#home', via: 'get'
+  match '/home',    to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   
-  match '/signup',  to: 'users#new', via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   
   get "static_pages/about"
   get "static_pages/home"
