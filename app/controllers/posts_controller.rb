41f5f5494acc95bbf37b0da3bb3c6d57204f3897
Brid Mackey <brid.mackey@gmail.com>
2013-12-08 16:38:09 +0000

    Adding posts from user

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  before_filter :signed_in_user, only: [:create, :destroy]
+   before_filter :correct_user,   only: :destroy
+
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "Post created!"
+      redirect_to home_path
+    else
+      @feed_items = []
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to home_path
+  end
+
+  private
+
+    def micropost_params
+      params.require(:post).permit(:content)
+    end
+
+    def correct_user
+      @post = current_user.posts.where(id: params[:id]).first
+      redirect_to root_url if @post.nil?
+    end
+end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,19 @@
 class StaticPagesController < ApplicationController
+
   def about
   end
+
   def help
   end
+
   def home
+    if signed_in?
+      @post  = current_user.posts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
+
   def contact
   end
+
 end
